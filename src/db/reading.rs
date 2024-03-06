7525fe04b724c2914b25eff3fe05836c12c29363
Theprotocolwhisperer <chrismata10@gmail.com>
2024-03-06 04:34:33 -0300

    (feature) : Get all Lisitings by nft_address

--- src/db/reading.rs
+++ src/db/reading.rs
@@ -1,19 +1,32 @@
 use serde::{Deserialize, Serialize};
-use sqlx::{PgPool, Error, FromRow};
 use sqlx::postgres::PgRow;
 use sqlx::Row;
+use sqlx::{Error, FromRow, PgPool};
 
 #[derive(Debug, Serialize, Deserialize, FromRow)]
-pub struct Listing {
+pub struct FloorPrice {
     listing_id: i64,
     nft_address: String,
-    min_price: String, 
-
+    min_price: String,
 }
 
-pub async fn get_lowest_priced_listing_for_address(pool: &sqlx::Pool<sqlx::Postgres>, nft_address: &str) -> Result<Option<Listing>, sqlx::Error> {
+#[derive(Debug, Serialize, Deserialize, FromRow)]
+pub struct PalletListing {
+    pub listing_id: i64,
+    pub nft_owner: String,
+    pub nft_address: String,
+    pub token_id: String,
+    pub min_price: String,
+    pub block_height: String,
+    pub txhash: String,
+    pub listed: bool,
+}
+pub async fn get_lowest_priced_listing_for_address(
+    pool: &sqlx::Pool<sqlx::Postgres>,
+    nft_address: &str,
+) -> Result<Option<FloorPrice>, sqlx::Error> {
     let result = sqlx::query_as!(
-        Listing,
+        FloorPrice,
         "SELECT listing_id, nft_address, min_price FROM \"public\".\"palletlistings\"
          WHERE listed = true AND nft_address = $1
          ORDER BY min_price ASC
@@ -22,6 +35,22 @@ pub async fn get_lowest_priced_listing_for_address(pool: &sqlx::Pool<sqlx::Postg
     )
     .fetch_optional(pool)
     .await?;
-    
+
+    Ok(result)
+}
+
+pub async fn get_listings_for_address(
+    pool: &sqlx::Pool<sqlx::Postgres>,
+    nft_address: &str,
+) -> Result<Vec<PalletListing>, sqlx::Error> {
+    let result = sqlx::query_as!(
+        PalletListing,
+        "SELECT * FROM \"public\".\"palletlistings\"
+         WHERE listed = true AND nft_address = $1",
+        nft_address
+    )
+    .fetch_all(pool)
+    .await?;
+
     Ok(result)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,31 @@
-use actix_web::{web, App, HttpServer, HttpResponse, Responder};
-use sqlx::postgres::PgPoolOptions;
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use dotenv::dotenv;
+use sqlx::postgres::PgPoolOptions;
 use std::env;
 
 mod db;
-use db::reading::get_lowest_priced_listing_for_address;
+use db::reading::{get_listings_for_address, get_lowest_priced_listing_for_address, PalletListing};
 
 // Assuming `db` module and `get_lowest_priced_listing_for_address` function are defined correctly elsewhere
 
-async fn get_floor_price(pool: web::Data<sqlx::Pool<sqlx::Postgres>>, nft_address: web::Path<String>) -> impl Responder {
+async fn get_floor_price(
+    pool: web::Data<sqlx::Pool<sqlx::Postgres>>,
+    nft_address: web::Path<String>,
+) -> impl Responder {
     match get_lowest_priced_listing_for_address(&pool, &nft_address).await {
         Ok(Some(listing)) => HttpResponse::Ok().json(listing),
         Ok(None) => HttpResponse::NotFound().finish(),
+        Err(_) => HttpResponse::InternalServerError().finish(),
+    }
+}
+
+async fn get_listings_from_collection(
+    pool: web::Data<sqlx::Pool<sqlx::Postgres>>,
+    nft_address: web::Path<String>,
+) -> impl Responder {
+    match get_listings_for_address(&pool, &nft_address.into_inner()).await {
+        Ok(listings) if !listings.is_empty() => HttpResponse::Ok().json(listings),
+        Ok(_) => HttpResponse::NotFound().finish(),
         Err(_) => HttpResponse::InternalServerError().finish(),
     }
 }
@@ -35,7 +49,14 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .app_data(web::Data::new(db_pool.clone()))
             .route("/", web::get().to(index)) // Changed to `get` for the demonstration
-            .route("/pallet_floorprice_collection/{nft_address}", web::get().to(get_floor_price))
+            .route(
+                "/pallet_floorprice_collection/{nft_address}",
+                web::get().to(get_floor_price),
+            )
+            .route(
+                "/listings_collection/{nft_address}",
+                web::get().to(get_listings_from_collection),
+            )
     })
     .bind("127.0.0.1:8080")?
     .run()
