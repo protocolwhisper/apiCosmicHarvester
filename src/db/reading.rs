1509d5bf2e850917cbe779256a3b7f99eee14e5a
protocolwhisper <chrismata10@gmail.com>
2024-03-06 00:30:52 -0500

    Cleaning file

--- src/db/reading.rs
+++ src/db/reading.rs
@@ -5,11 +5,10 @@ use sqlx::Row;
 
 #[derive(Debug, Serialize, Deserialize, FromRow)]
 pub struct Listing {
-    // Ensure these fields match your database schema
     listing_id: i64,
     nft_address: String,
-    min_price: String, // Adjust the type according to your schema
-    // Add other fields as necessary
+    min_price: String, 
+
 }
 
 pub async fn get_lowest_priced_listing_for_address(pool: &sqlx::Pool<sqlx::Postgres>, nft_address: &str) -> Result<Option<Listing>, sqlx::Error> {
